eb7c00775ca0febd42d1bcd1a8d1e4770f703346
Sam Myers <sam@students.olin.edu>
2017-12-13 15:32:04 -0500

    Add all the stuff.

--- index.js
+++ index.js
@@ -1,44 +1,90 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 const SerialPort = require('serialport');
-const Readline = SerialPort.parsers.Readline;
+const ByteLength = SerialPort.parsers.ByteLength;
 
 const PORT = 3000;
 const SERIAL_DEVICE = process.argv[2];
 const SERIAL_BAUD_RATE = 115200;
 
 const app = express();
+app.use(bodyParser.json());
 const port = new SerialPort(SERIAL_DEVICE, { baudRate: SERIAL_BAUD_RATE }, false);
-const parser = port.pipe(new Readline());
+const parser = port.pipe(new ByteLength({ length: 1 }));
 
 const jobQueue = [];
 
+const lanternOpts = {
+  lanternMode: ['off', 'on', 'pulse', 'sensory'],
+  lightMode: ['off', 'on', 'pulse', 'mirror'],
+  pulseSpeed: [1, 2, 3, 4],
+};
+
+function lanternsToBitField(flags) {
+  return flags.reduce((bitField, flag) => bitField | 1 << flag, 0);
+}
+
+function optionsToBitField(requestOpts) {
+  const { lanternMode, lightMode, lightColor } = _.mapValues(
+    _.pick(requestOpts, _.keys(lanternOpts)),
+    (value, key) => lanternOpts[key].indexOf(value)
+  );
+  const optArray = [lanternMode, lightMode, lightColor];
+  if (optArray.includes(-1)) {
+    throw new Error('Unknown option.');
+  }
+  return optArray.reduce((bitField, opt, index) => bitField | opt << 2 * index, 0);
+}
+
+function bitFieldToStatus(bitField) {
+  return ['lanternMode', 'lightMode', 'pulseSpeed'].reduce(
+    (opts, opt, idx) => {
+      const optIndex = (bitField >> (2 * idx)) & 0x3;
+      opts[opt] = lanternOpts[opt][optIndex];
+      return opts; 
+    }, {}
+  );
+}
+
 function writeToSerial(command, callback) {
-  port.write(command, 'ascii', err => {
+  if (callback) {
+    jobQueue.unshift(callback);
+  }
+  port.write(command, 'binary', err => {
     if (err) {
       console.log('Error writing to serial port.');
       return;
     }
-    console.log(command, 'command written to serial port.');
-    if (callback) callback();
-  })
+    console.log('Command written to serial port.');
+  });
+}
+
+function writeLanternUpdate(opts, callback) {
+  if (!opts) {
+    throw new Error();
+  }
+  const command = Buffer.from([optionsToBitField(opts)]);
+  writeToSerial(command, callback);
 }
 
 app.get('/status', (request, response) => {
-  writeToSerial('STATUS', () => {
-    jobQueue.unshift(status => {
-      response.json({ status, color: 'none' })
-    });
+  console.log('Status');
+  writeToSerial(Buffer.from([255]), status => {
+    response.json(bitFieldToStatus(status));
   });
 });
 
-app.post('/on', (request, response) => {
-  writeToSerial('ON');
-  response.send('Command sent.');
-});
-
-app.post('/off', (request, response) => {
-  writeToSerial('OFF');
-  response.send('Command sent.');
+app.post('/update', (request, response) => {
+  let success;
+  try {
+    writeLanternUpdate(request.body, status => {
+      response.json(bitFieldToStatus(status));
+    });
+  } catch (e) {
+    console.log(e);
+    response.status(400).send('Malformed lantern command.');
+  }
 });
 
 app.listen(PORT, err => {
@@ -49,9 +95,9 @@ app.listen(PORT, err => {
   console.log(`Server is listening on port ${PORT}.`);
 })
 
-port.on('data', data => {
+port.on('data', buf => {
   const callback = jobQueue.pop();
-  const dataString = data.toString('ascii').trim();
-  callback(dataString);
-  console.log('Arduino output:', dataString);
+  const data = buf.readInt8(0);
+  if (callback) callback(data);
+  console.log('Arduino output:', data);
 });
--- musclewire/musclewire.ino
+++ musclewire/musclewire.ino
@@ -1,30 +1,174 @@
 const int WIRE_PIN = 3;
+const int SENSOR_PIN = A0;
+const int LED_PIN = 9;
 const long BAUD_RATE = 115200;
 
-int state = LOW;
+const int pulseInterval = 6500;
+const int pwmPulseInterval = pulseInterval / 100;
+const int sensorThreshold = 350;
+
+// lanternMode:  | lightMode:   | pulseSpeed
+//   0: off      |   0: off     |   0: 1
+//   1: on       |   1: on      |   1: 2
+//   2: pulse    |   2: pulse   |   2: 3
+//   3: sensory  |   3: mirror  |   3: 4
+
+
+struct LanternState {
+  byte lanternMode;
+  byte lightMode;
+  byte pulseSpeed;
+};
+
+unsigned long previousMillis = 0;
+int wireState = LOW;
+LanternState lanternState = { 0, 0, 0 };
+
+int pulseState = LOW;
+int pwmPulseState = 0;
+
+/*
+ * Calls provided function with the provided `data` argument if a given
+ * interval (in milliseconds) has elapsed.
+ */
+void executeIfTimeElapsed(void (*callback)(void *), int interval, void *data) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    callback(data);
+  }
+}
+
+LanternState parseCommand(byte optBitField) {
+  byte pulseSpeed = (optBitField >> 4) & B11;
+  byte lightMode = (optBitField >> 2) & B11;
+  byte lanternMode = optBitField & B11;
+  return (LanternState) { lanternMode, lightMode, pulseSpeed };
+}
+
+byte encodeState(LanternState state) {
+ return (state.pulseSpeed << 4) | (state.lightMode << 2) | state.lanternMode;
+}
+
+void writeStatus() {
+  Serial.write(encodeState(lanternState));
+}
+
+void toggleLantern() {
+  updateLightPWM();
+  digitalWrite(WIRE_PIN, pulseState);
+}
+
+void pulseLantern() {
+  executeIfTimeElapsed(toggleLantern, pwmPulseInterval, NULL);
+}
+
+void updateLightPWM() {
+  if (pwmPulseState == 99) {
+    pulseState = LOW;
+  } else if (pwmPulseState == 0) {
+    pulseState = HIGH;
+  }
+  if (pulseState == LOW) {
+    pwmPulseState--;
+  } else if (pulseState == HIGH) {
+    pwmPulseState++;
+  }
+}
+
+void sensorUpdate() {
+
+}
+
+void pulseLight() {
+  executeIfTimeElapsed(updateLightPWM, pwmPulseInterval, NULL);
+  analogWrite(LED_PIN, map(pwmPulseState, 0, 99, 0, 255));
+}
+
+void updateLight() {
+  switch (lanternState.lightMode) {
+    case 0:
+      analogWrite(LED_PIN, 0);
+      break;
+    case 1:
+      analogWrite(LED_PIN, 255);
+      break;
+    case 2:
+      pulseLight();
+      break;
+    case 3:
+      switch (lanternState.lanternMode) {
+        case 0:
+          analogWrite(LED_PIN, 0);
+          break;
+        case 1:
+          analogWrite(LED_PIN, 255);
+          break;
+        case 2:
+          pulseLight();
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void updateLantern() {
+  switch (lanternState.lanternMode) {
+    case 0:
+      digitalWrite(WIRE_PIN, LOW);
+      break;
+    case 1:
+      digitalWrite(WIRE_PIN, HIGH);
+      break;
+    case 2:
+      pulseLantern();
+      break;
+    case 3:
+      sensorUpdate();
+      break;
+    default:
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(BAUD_RATE);
 
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(WIRE_PIN, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
-    if (command == "ON") {
-      state = HIGH;
-    } else if (command == "OFF") {
-      state = LOW;
-    } else if (command == "STATUS") {
-      if (state == HIGH) {
-        Serial.println("ON");
-      } else {
-        Serial.println("OFF");
+    int opts = Serial.read();
+    // Check if first bit is 1, if so just return current status
+    if (!(opts >> 7)) {
+      LanternState command = parseCommand(opts);
+      bool newLanternMode = lanternState.lanternMode != command.lanternMode;
+      bool newLightMode = lanternState.lightMode != command.lightMode;
+      bool lanternPulse = command.lanternMode == 2;
+      bool lightPulse = command.lightMode == 2;
+      bool lightMirror = command.lightMode == 3;
+      if (newLanternMode && lanternPulse && !lightPulse) {
+        pwmPulseState = 0;
+        pulseState = LOW;
       }
+      if (newLightMode && lightPulse && !lanternPulse) {
+        pwmPulseState = 0;
+        pulseState = LOW;
+      }
+      if (command.lanternMode == 3) {
+        command.lightMode = 3;
+      }
+      lanternState = command;
     }
+    writeStatus();
   }
-  digitalWrite(WIRE_PIN, state);
-  digitalWrite(LED_BUILTIN, state);
+  updateLantern();
+  updateLight();
 }
